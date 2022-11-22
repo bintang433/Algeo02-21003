827a60fa7be33b2ac1168ed0043b408d0aa5dec7
christodharma <13521009@std.stei.itb.ac.id>
2022-11-22 18:03:12 +0700

    inputweight, datasetweight

--- src/functions.py
+++ src/functions.py
@@ -10,13 +10,6 @@ import cv2
 def createMatrix(row, col):
     Matrix = [[0 for i in range(col)] for j in range(row)]
     return Matrix
-def createIdentity(row,col):
-    Matrix = createMatrix(row,col)
-    for i in range(row):
-        for j in range(col):
-            if (i==j):
-                Matrix[i][j] = 1
-    return Matrix
 
 # {Fungsi selektor 1 baris suatu Matrix}
 # asList False akan mengembalikan matrix dimensi 1xCol yang merupakan baris matrix dengan index row
@@ -44,12 +37,7 @@ def getCol(Matrix, col, asList):
 
 # {fungsi yang menghasilkan salinan suatu matriks}
 def copyMatrix(Matrix):
-    rowCopy = len(Matrix)
-    colCopy = len(Matrix[0])
-    copy = [[0 for j in range(colCopy)] for i in range(rowCopy)]
-    for i in range(rowCopy):
-        for j in range(colCopy):
-            copy[i][j] = Matrix[i][j]
+    copy = np.copy(Matrix)
     return copy
 
 # {Prosedur print matrix}
@@ -85,12 +73,6 @@ def multiplyMatrix(Matrix1, Matrix2):
     result = np.matmul(Matrix1, Matrix2)
     return result
 
-# {fungsi yang mengembalikan hasil kali Matrix1 dan Matrix2 dan dibentuk menjadi matrix ukuran }
-def reshapeMultiply(Matrix1, Matrix2, row, col):
-    np.array(Matrix1).reshape(row,col)
-    result = multiplyMatrix(Matrix1, Matrix2)
-    return result
-
 # {fungsi yang mengembalikan transpose matrix}
 # fungsi ini menerima matrix lalu mengembalikan hasil transpose matrix tersebut
 def transpose(Matrix):
@@ -108,31 +90,14 @@ def transpose(Matrix):
             Mtr[k][i] = temp[k]
     return Mtr
 
-# {fungsi yang mengembalikan konkatenasi dari 2 matrix atau 
-# lebih tepatnya menghasilkan matrix1 diaugmentasi dengan matrix2}
-def concatMatrix(Matrix1, Matrix2):
-    M1 = Matrix1
-    M2 = Matrix2
-    resultCol = len(M1[0])+len(M2[0])
-    rowM1 = len(M1)
-    colM1 = len(M1[0])
-    result = createMatrix(rowM1, resultCol)
-    for i in range(rowM1):
-        for j in range(resultCol):
-            if (j<colM1):
-                result[i][j] = M1[i][j]
-            else :
-                result[i][j] = M2[i][j-colM1]
-    return result
-
 # {fungsi yang mengembalikan suatu matrix yang dijadikan 1 baris dengan konkatenasi}
 # hasil yang dikembalikan adalah matrix berdimensi 1xCol yang merupakan seluruh elemen matrix yang dijadikan satu baris
-def intoOneRow(Matrix):
+def intoOneRowMat(Matrix):
     oneRow = [np.array(Matrix).flatten()]
     return oneRow
 # {fungsi yang mengembalikan suatu matrix yang dijadikan 1 kolom dengan konkatenasi dan transpose}
-def intoOneCol(Matrix):
-    return transpose(intoOneRow(Matrix))
+def intoOneColMat(Matrix):
+    return transpose(intoOneRowMat(Matrix))
 
 # Operasi baris matrix
 # baris : 1 baris matrix yang dijadikan array (1 dimensi)
@@ -158,14 +123,6 @@ def multiplyByConstRow(baris, n):
 
 # Operasi vektor
 # Vektor : 1 kolom matrix yang dijadikan array (1 dimensi)
-# {fungsi selektor matrix untuk mendapatkan 1 vektor dr matrix}
-def getVector(Matrix, col):
-    row = len(Matrix)
-    result = [0 for i in range(row)]
-    for i in range(row):
-        result[i] = Matrix[i][col]
-    return result
-
 # {fungsi yang menghasilkan penjumlahan dua vektor u dan vektor v}
 def addVector(u, v):
     result = np.add(u,v)
@@ -193,17 +150,77 @@ def magnitudeVector(u):
         result += u[i]*u[i]
     return (sqrt(result))
 
-# {return magnitude matrix m, sqrt(sum(sum(i**2 for i in j) for j in matrix))}
-def magnitudeMatrix(m):
-    result = 0
-    for i in range(len(m)):
-        for j in range(len(i)):
-            result += m[i][j]**2
-    return result**0.5
-
 # {fungsi yang mengembalikan proyeksi vektor u pada v}
 def orthoProjectVector(u, v):
     return (scaleVector(u, dotProductVector(u,v)/dotProductVector(u,u)))
+
+# Operator Image
+# {mengakses semua image di dalam image dan mengeluarkan matrix grayscale yang diresize}
+def accessImage (folder):
+    tempImg = cv2.imread(folder, cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(tempImg, [256, 256])
+    ar = np.array(image)
+    matrix = intoOneRowMat(ar)
+    return matrix
+
+# {fungsi yang mengakses dataset, mengubah image-image menjadi ukuran tertentu, grayscale dan menjadi 1 baris, lalu mengonkatenasi semuanya untuk diproses}
+def datasetToArray (folder):
+    result = []
+    for (root,dirs,files) in os.walk(folder, topdown=True):
+        for i in files:
+            directory = root + "\\" + i
+            tempImg = cv2.imread(directory, cv2.IMREAD_GRAYSCALE)
+            image = cv2.resize(tempImg, [256, 256])
+            ar = np.array(image)
+            matrix = asarray(ar)
+            result.append(np.array(matrix).flatten())
+    return result
+# {fungsi yang mengakses dataset, mengubah image-image menjadi ukuran tertentu, grayscale dan menjadi 1 baris, lalu mengonkatenasi semuanya untuk diproses}
+# {fungsi ini sama dengan dataSetToArray() tapi menambahkan batasan jumlah foto}
+# {untuk tidak membatasi banyak matrix yang dihasilkan, amount diinput -1}
+def datasetToArray_FixedAmount (folder, amount):
+    result = []
+    ctr=0
+    for (root,dirs,files) in os.walk(folder, topdown=True):
+        for i in files:
+            if ctr>=amount and amount>=0:
+                break
+            directory = root + "\\" + i
+            tempImg = cv2.imread(directory, cv2.IMREAD_GRAYSCALE)
+            image = cv2.resize(tempImg, [256, 256])
+            ar = np.array(image)
+            matrix = asarray(ar)
+            result.append(np.array(matrix).flatten())
+            ctr+=1
+        ctr=0
+    return result
+
+#{fungsi yang mengembalikan matrix kovarian dari suatu matrix}
+def deltaMeanAndCovariant(Matrix):
+    print(f"ukuran Matrix {np.array(Matrix).shape}")
+    dataset = copyMatrix(Matrix)
+    row = len(Matrix)
+    col = len(Matrix[0])
+    #menjumlahkan data-data
+    mean = [0 for j in range(col)]
+    for i in range(row):
+        mean = addRow(mean, dataset[i])
+    #dataset dibagi banyak data
+    mean = multiplyByConstRow(mean, 1/row)
+    meanVector = [mean]
+    print("Achieved mean of matrices")
+    #dari sini sudah diperoleh nilai mean dari row-row
+    #mengurangi row-row dengan mean
+    for i in range(row):
+        dataset[i] = subtractRow(dataset[i], mean)
+    print("Achieved dataset mean")
+    #dari sini dataset adalah berukuran baris banyak file kolom resolusi**2
+    #mendapatkan matrix kovarian
+    cov = multiplyMatrix(dataset, transpose(dataset))
+    # cov = multiplyMatrix(dataset, transpose(dataset))
+    print("Achieved covariance matrix")
+    #ukuran matrix kovarian: banyak file x banyak file
+    return transpose(dataset), transpose(meanVector), cov
 
 # {fungsi yang menghasilkan dekomposisi QR dari suatu matrix}
 def QR(Matrix):
@@ -211,10 +228,10 @@ def QR(Matrix):
     row = len(Matrix)
     col = len(Matrix[0])
     for j in range(col):
-        u = getVector(Q, j)
-        v = getVector(Q, j)
+        u = getCol(Q, j, True)
+        v = getCol(Q, j, True)
         for k in range(j-1,0-1,-1):
-            uk = getVector(Q, k)
+            uk = getCol(Q, k, True)
             u = subtractVector(u, orthoProjectVector(uk,v))
         for i in range(col):
             Q[i][j] = u[i] / magnitudeVector(u)
@@ -249,114 +266,8 @@ def eigenvector(Matrix, EigenVal):
         for j in range(len(EigenVal)):
             tempMat[j][j] -= tempEigVal[j]
         EigVec = np.linalg.solve(tempMat, tempEigVal)
-        norm = magnitudeVector(EigVec)
-        # for j in range(len(EigVec)):
-        #     EigVec[j] /= norm
         result.append(EigVec)
     return result
-
-# {fungsi copy matrix 3D}
-def copyMatrix3D (Matrix3D):
-    col = len(Matrix3D[0][0])
-    row = len(Matrix3D[0])
-    depth = len(Matrix3D)
-    result = [[[0 for j in range(col)] for i in range(row)] for k in range(depth)]
-    for k in range(depth):
-        for i in range(row):
-            for j in range(col):
-                result[k][i][j] = Matrix3D[k][i][j]
-    return result
-
-# Operator Image
-# {mengakses semua image di dalam image dan mengeluarkan matrix grayscale yang diresize}
-def accessImage (folder):
-    tempImg = cv2.imread(folder, cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(tempImg, [256, 256])
-    ar = np.array(image)
-    matrix = asarray(ar)                #matrix = masing-masing gambar dalam bentuk matrix
-    matrix = intoOneCol(matrix)
-    return matrix
-# def accessImage (folder):
-#     for (root,dirs,files) in os.walk(folder, topdown=True):
-#         for i in files:
-#             directory = root + "\\" + i
-#             image = Image.open(directory,mode='r').convert('L').resize([256,256])
-#             ar = np.array(image)
-#             matrix = asarray(ar)                #matrix = masing-masing gambar dalam bentuk matrix
-#             # print(matrix)
-
-# {fungsi yang mengakses dataset, mengubah image-image menjadi ukuran tertentu, grayscale dan menjadi 1 baris, lalu mengonkatenasi semuanya untuk diproses}
-def datasetToArray (folder):
-    result = []
-    for (root,dirs,files) in os.walk(folder, topdown=True):
-        for i in files:
-            directory = root + "\\" + i
-            tempImg = cv2.imread(directory, cv2.IMREAD_GRAYSCALE)
-            image = cv2.resize(tempImg, [256, 256])
-            ar = np.array(image)
-            matrix = asarray(ar)
-            result.append(matrix)
-    return result
-# {fungsi yang mengakses dataset, mengubah image-image menjadi ukuran tertentu, grayscale dan menjadi 1 baris, lalu mengonkatenasi semuanya untuk diproses}
-# {fungsi ini sama dengan dataSetToArray() tapi menambahkan batasan jumlah foto}
-# {untuk tidak membatasi banyak matrix yang dihasilkan, amount diinput -1}
-def datasetToArray_FixedAmount (folder, amount):
-    result = []
-    ctr=0
-    for (root,dirs,files) in os.walk(folder, topdown=True):
-        for i in files:
-            if ctr>=amount and amount>=0:
-                break
-            directory = root + "\\" + i
-            tempImg = cv2.imread(directory, cv2.IMREAD_GRAYSCALE)
-            image = cv2.resize(tempImg, [256, 256])
-            ar = np.array(image)
-            matrix = asarray(ar)
-            result.append(matrix)
-            ctr+=1
-        ctr=0
-    return result
-
-# {menghitung jumlah file}
-def numberOfImage (folder):
-    sum = 0
-    for (root,dirs,files) in os.walk(folder, topdown=True):
-        for i in files:
-            sum+=1
-    return sum
-
-#{fungsi yang mengembalikan matrix kovarian dari suatu matrix}
-def deltaMeanAndCovariant (Matrix3D):
-    dataset = copyMatrix3D(Matrix3D)
-    depth = len(Matrix3D)
-    row = len(Matrix3D[0])
-    col = len(Matrix3D[0][0])
-    delta = []
-    #flatten matrix 3D
-    for i in range(depth):
-        temp = np.array(dataset[i]).flatten()
-        delta.append(temp)
-    #menjumlahkan data-data
-    mean = [0 for j in range(col*row)]
-    for i in range(depth):
-        mean = addRow(mean, delta[i])
-
-    #delta dibagi banyak data
-    mean = multiplyByConstRow(mean, 1/depth)
-    meanVector = [mean]
-    print("Achieved mean of matrices")
-    #dari sini sudah diperoleh nilai mean dari row-row
-    #mengurangi row-row dengan mean
-    for i in range(depth):
-        delta[i] = subtractRow(delta[i], mean)
-    print("Achieved delta mean")
-    #dari sini delta adalah berukuran baris banyak file kolom resolusi**2
-    #mendapatkan matrix kovarian
-    cov = multiplyMatrix(delta, transpose(delta))
-    # cov = multiplyMatrix(delta, transpose(delta))
-    print("Achieved covariance matrix")
-    #ukuran matrix kovarian: banyak file x banyak file
-    return transpose(delta), transpose(meanVector), cov
 
 def eigenFaces(eigenVectors, deltaMean):
     eigFaces = []
@@ -370,7 +281,7 @@ def eigenFaces(eigenVectors, deltaMean):
     eigFaces = transpose(eigFaces)
     return eigFaces
 
-def Omega(faces, deltaMean):
+def omega(faces, deltaMean):
     omegaMat = multiplyMatrix(transpose(deltaMean),faces)
     # for i in range(len(deltaMean[0])):
     #     for j in range(len(faces[0])):
@@ -388,3 +299,48 @@ def Omega(faces, deltaMean):
 #             currMin = distance
 #             face = y
 #     return face.reshape(256, 256)
+
+def datasetWeight(folder, fileAmount, eigenIteration):
+    MATRIX = datasetToArray_FixedAmount(os.getcwd()+folder, fileAmount)
+    deltaMean, meanMATRIX, covMATRIX = deltaMeanAndCovariant(MATRIX)
+    eigenValues = eigenvalue(covMATRIX, eigenIteration)
+    total = sum(eigenValues)
+    if (len(MATRIX[0]) > 5):
+        temp = 0
+        i = 0
+        eigenValFilter = []
+        while (temp<(0.95*total))and(eigenValues[i]>=1):
+            eigenValFilter.append(eigenValues[i])
+            temp += eigenValues[i]
+            i += 1
+    else : 
+        eigenValFilter = eigenValues
+    eigVecEff = len(eigenValFilter)
+    print(f"ukuran eigenValFilter: {len(eigenValFilter)}")
+    eigenVectors = eigenvector(covMATRIX, eigenValues)
+    print(f"Ukuran eigenVector: {np.array(eigenVectors).shape}")
+    tempEigenFaces = eigenFaces(eigenVectors, deltaMean)
+    eigenfaces = transpose(transpose(tempEigenFaces)[:eigVecEff])
+    print(f"ukuran eigenFace: {len(eigenfaces)} x {len(eigenfaces[0])}")
+    result = omega(eigenfaces, deltaMean)
+    print(f"ukuran result {np.array(result).shape}")
+    return result
+
+def inputWeight(Matrix1Row):
+    Matrix = np.array(Matrix1Row).reshape(256,256)
+    print(f"ukuran Matrix {np.array(Matrix).shape}")
+    dataset = copyMatrix(Matrix)
+    row = len(Matrix)
+    col = len(Matrix[0])
+    mean = [0 for j in range(col)]
+    for i in range(row):
+        mean = addRow(mean, dataset[i])
+    mean = multiplyByConstRow(mean, 1/row)
+    meanVector = [mean]
+    for i in range(row):
+        dataset[i] = subtractRow(dataset[i], mean)
+    cov = multiplyMatrix(dataset, transpose(dataset))
+    eigenVectors = eigenvector(cov)
+    eigenfaces = eigenFaces(eigenVectors, dataset)
+    weight = omega(eigenfaces, dataset)
+    return weight
